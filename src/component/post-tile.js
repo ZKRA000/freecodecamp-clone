352b82c8bd61e6a25cf228b6aa687001b9464f3b
ZKRA000 <44751014+ZKRA000@users.noreply.github.com>
2022-10-15 12:03:34 +0700

    push

--- src/component/post-tile.js
+++ src/component/post-tile.js
@@ -2,16 +2,16 @@ import user from '../img/user.jpg'
 import thumbnail from '../img/thumbnail.jpg'
 import '../css/post-tile.css'
 
-function PostTile() {
+function PostTile(props) {
     return (
     <div className="post-tile">
         <img src={thumbnail} alt="" className="post-tile__thumbnail" />
         <div>
-            <p>#TYPESCRIPT</p>
-            <h2>TypeScript Generics – Use Case and Examples</h2>
+            <p>{props.tag}</p>
+            <h2>{props.title}</h2>
             <div className="post-tile__bottom">
                 <img src={user} alt="" className="post-tile__user" />
-                <div>2 DAYS AGO</div>
+                <div>{props.time}</div>
             </div>
         </div>
     </div>
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import PostTile from '../component/post-tile'
+import '../css/posts.css'
 
 function Posts() {
     return (
         <React.Fragment>
-            <section className='container'>
-                <PostTile />
-                <PostTile />
-                <PostTile />
-                <PostTile />
+            <section className='container posts-container'>
+            {[...Array(3)].map(v => {
+                return <PostTile 
+                            tag="#TYPESCRIPT"
+                            title="TypeScript Generics - Use Case and Examples"
+                            time="2 DAYS AGO" 
+                        />
+            })}
             </section>
         </React.Fragment>
     )
